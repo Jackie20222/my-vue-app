cd66d66bd85e07082087f88d31b470127b0577cb
Jackie20222 <604673138@qq.com>
2023-02-11 18:24:59 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div >
+ 
+    <Navtion/>
+    <Search/>
+    <foot/>
+  </div>
+
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld'
+import Navtion  from './components/Nav.vue'
+import Search  from './components/search.vue'
+import foot  from './components/foot.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Navtion,Search,foot
+  }
+}
+</script>
+
+<style>
+
+</style>
+
